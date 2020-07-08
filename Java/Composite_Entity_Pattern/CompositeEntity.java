d381da4a869b2e5f2a830dee958c3e89982d62bc
Fernando Calmet <fercalmet@gmail.com>
2020-07-08 11:10:59 -0500

    composite entity pattern

---
+++ Java/Composite_Entity_Pattern/CompositeEntity.java
@@ -0,0 +1,13 @@
+package Java.Composite_Entity_Pattern;
+
+public class CompositeEntity {
+    private CoarseGrainedObject cgo = new CoarseGrainedObject();
+
+    public void setData(String data1, String data2) {
+        cgo.setData(data1, data2);
+    }
+
+    public String[] getData() {
+        return cgo.getData();
+    }
+}
---
+++ Java/Composite_Entity_Pattern/DependentObject2.java
@@ -0,0 +1,14 @@
+package Java.Composite_Entity_Pattern;
+
+public class DependentObject2 {
+
+    private String data;
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}

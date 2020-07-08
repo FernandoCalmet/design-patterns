9d1cdfedcbaa54e0a0a54f28e788fad8fba17842
Fernando Calmet <fercalmet@gmail.com>
2020-07-08 09:10:18 -0500

    mediator pattern

---
+++ Java/Mediator_Pattern/ChatRoom.java
@@ -0,0 +1,9 @@
+package Java.Mediator_Pattern;
+
+import java.util.Date;
+
+public class ChatRoom {
+    public static void showMessage(User user, String message) {
+        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+    }
+}
---
+++ Java/Mediator_Pattern/MediatorPatternDemo.java
@@ -0,0 +1,11 @@
+package Java.Mediator_Pattern;
+
+public class MediatorPatternDemo {
+    public static void main(String[] args) {
+        User robert = new User("Robert");
+        User john = new User("John");
+
+        robert.sendMessage("Hi! John!");
+        john.sendMessage("Hello! Robert!");
+    }
+}

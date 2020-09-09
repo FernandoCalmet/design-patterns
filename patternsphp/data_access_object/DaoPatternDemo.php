5ec5bdb3de623be75db9541f6e3b4fb9519cb4d1
Fernando Calmet <fercalmet@gmail.com>
2020-09-09 01:53:37 -0500

    update

--- patternsphp/data_access_object/DaoPatternDemo.php
+++ patternsphp/data_access_object/DaoPatternDemo.php
@@ -3,21 +3,20 @@ require_once __DIR__ . '/Student.php';
 require_once __DIR__ . '/StudentDao.php';
 require_once __DIR__ . '/StudentDaoImpl.php';
 
-$student = new Student();
 $studentDao = new StudentDaoImpl();
 $students = $studentDao->getAllStudents();
 
 //print all students
-foreach ($student as $students) {
-    print sprintf("Student: [RollNo: %s , Name: %s]", $student->getRollNo(), $student->getName() . PHP_EOL);
+foreach ($students as $student) {
+    print sprintf("Student: [RollNo: %s , Name: %s]", $student->getRollNo(), $student->getName() . PHP_EOL);   
 }
 
 //update student
-$student = $studentDao->getAllStudents()->GetObj(0);
+$student = $studentDao->getStudent(0);
 $student->setName("Michael");
 $studentDao->updateStudent($student);
 
 //get the student
-$studentDao->getStudent(0);
+$student = $studentDao->getStudent(0);
 print(PHP_EOL);
 print sprintf("Student: [RollNo: %s , Name: %s]", $student->getRollNo(), $student->getName());
